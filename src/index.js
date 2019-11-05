806e59e51c7aa253a354a2189e6886c2e6fc59f0
Olga Ioffe <yoffic@yahoo.com>
2019-11-05 12:31:43 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}`);
+const sayHello = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
+export default sayHello;
