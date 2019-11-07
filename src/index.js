cab83729f103110c9e990d781a0126201ee75c9f
Olga Ioffe <yoffic@yahoo.com>
2019-11-07 18:39:38 +0300

    create game interface

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import readlineSync from 'readline-sync';
+import { car, cdr } from '@hexlet/pairs';
 
-export const welcome = () => console.log('Welcome to the Brain Games!');
-export const instructIsEven = () => console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
-let userName = '';
-export const sayHello = () => {
-  userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!
-  `);
+const makeGame = (rule, gameValues) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(rule);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+
+  const askQuestion = (round) => {
+    if (round > 3) {
+      return console.log(`Congratulations, ${userName}!`);
+    }
+    const currentGameValues = gameValues(round);
+    const questionValue = car(currentGameValues);
+    const correctAnswer = cdr(currentGameValues);
+    console.log(`Question: ${questionValue}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (correctAnswer === userAnswer) {
+      console.log('Correct!');
+      return askQuestion(round + 1);
+    }
+    return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+  };
+  return askQuestion(1);
 };
 
-// Brain-even game
-const generateRandomNumber = () => Math.floor(Math.random() * 100 + 1);
-const correctAnswer = (number) => (number % 2 === 0 ? 'yes' : 'no');
-
-export const askQuestion = (round) => {
-  if (round > 3) {
-    return console.log(`Congratulations, ${userName}!`);
-  }
-  const currentNumber = generateRandomNumber();
-  console.log(`Question: ${currentNumber}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  if (correctAnswer(currentNumber) === userAnswer) {
-    console.log('Correct!');
-    return askQuestion(round + 1);
-  }
-  return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer(currentNumber)}'.\nLet's try again, ${userName}!`);
-};
+export const generateRandomNumber = () => Math.floor(Math.random() * 100 + 1);
+export default makeGame;
