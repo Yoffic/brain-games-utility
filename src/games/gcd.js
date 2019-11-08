26b2e3dbd00f1d6e4a70816bee10a6dd44783785
Olga Ioffe <yoffic@yahoo.com>
2019-11-08 13:25:16 +0300

    add gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,24 @@
+import { cons } from '@hexlet/pairs';
+import makeGame, { generateRandomNumber } from '../index';
+
+const rule = 'Find the greatest common divisor of given numbers.\n';
+const getGreatestCommonDividor = (firstNumber, secondNumber) => {
+  let divider = firstNumber;
+  if (firstNumber > secondNumber) {
+    divider = secondNumber;
+  }
+  let result = 0;
+  for (let i = 1; i <= divider; i += 1) {
+    result = firstNumber % i === 0 && secondNumber % i === 0 ? i : result;
+  }
+  return result;
+};
+const createGameValues = () => {
+  const number1 = generateRandomNumber(100);
+  const number2 = generateRandomNumber(100);
+  const currentValue = `${number1} ${number2}`;
+  const correctAnswer = getGreatestCommonDividor(number1, number2);
+  return cons(currentValue, correctAnswer.toString());
+};
+
+export default () => makeGame(rule, createGameValues);
