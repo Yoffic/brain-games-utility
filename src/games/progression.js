2cd29ab88a5f13e87228cf3fc98e4e79b721e3f0
Olga Ioffe <yoffic@yahoo.com>
2019-11-09 18:33:55 +0300

    add progression game

---
+++ src/games/progression.js
@@ -0,0 +1,29 @@
+import { cons } from '@hexlet/pairs';
+import makeGame, { generateRandomNumber } from '../index';
+
+const rule = 'What number is missing in the progression?\n';
+const createProgression = () => {
+  const positionOfHiddenNumber = generateRandomNumber(10);
+  let progressionNumber = generateRandomNumber(100);
+  let progression = `${progressionNumber}`;
+  let correctAnswer = '';
+  if (positionOfHiddenNumber === 1) {
+    progression = '..';
+    correctAnswer = progressionNumber.toString();
+  } else {
+    for (let i = 1; i < positionOfHiddenNumber - 1; i += 1) {
+      progressionNumber += 2;
+      progression = `${progression} ${progressionNumber}`;
+      correctAnswer = (progressionNumber + 2).toString();
+    }
+    progression = `${progression} ..`;
+    progressionNumber += 2;
+  }
+  for (let i = positionOfHiddenNumber; i < 10; i += 1) {
+    progressionNumber += 2;
+    progression = `${progression} ${progressionNumber}`;
+  }
+  return cons(progression, correctAnswer);
+};
+
+export default () => makeGame(rule, createProgression);
