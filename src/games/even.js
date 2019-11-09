f44d37d442fc09f810977698f7b5de90d35228b4
Olga Ioffe <yoffic@yahoo.com>
2019-11-09 23:11:17 +0300

    update even game

--- src/games/even.js
+++ src/games/even.js
@@ -2,10 +2,11 @@ import { cons } from '@hexlet/pairs';
 import makeGame, { generateRandomNumber } from '../index';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".\n';
-const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => number % 2 === 0;
 const createGameValues = () => {
   const currentNumber = generateRandomNumber(100);
-  return cons(currentNumber, isEven(currentNumber));
+  const correctAnswer = isEven(currentNumber) ? 'yes' : 'no';
+  return cons(currentNumber, correctAnswer);
 };
 
 const evenGame = () => makeGame(rule, createGameValues);
