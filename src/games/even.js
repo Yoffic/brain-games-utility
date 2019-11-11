bafa32aefcfad047d11c17796c3a118a568b7583
Olga Ioffe <yoffic@yahoo.com>
2019-11-11 21:14:21 +0300

    refactor even.js

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,7 @@ import makeGame, { generateRandomNumber } from '../index';
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".\n';
 const isEven = (number) => number % 2 === 0;
 const createGameValues = () => {
-  const gameQuestion = generateRandomNumber(100);
+  const gameQuestion = generateRandomNumber(1, 100);
   const correctAnswer = isEven(gameQuestion) ? 'yes' : 'no';
   return cons(gameQuestion, correctAnswer);
 };
