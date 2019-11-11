6731dbddff43cff7426dea4e0fd185c68658df9e
Olga Ioffe <yoffic@yahoo.com>
2019-11-11 21:08:40 +0300

    refactor calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,26 +3,31 @@ import makeGame, { generateRandomNumber } from '../index';
 
 const rule = 'What is the result of the expression?\n';
 const operands = '+-*';
-const generateRandomOperand = () => {
-  const index = Math.floor(Math.random() * operands.length);
-  return operands[index];
-};
 const makeCalc = (number1, number2, operand) => {
-  if (operand === '+') {
-    return (number1 + number2);
+  let result = '';
+  switch (operand) {
+    case '+':
+      result = `${number1 + number2}`;
+      break;
+    case '-':
+      result = `${number1 - number2}`;
+      break;
+    case '*':
+      result = `${number1 * number2}`;
+      break;
+    default:
+      return null;
   }
-  if (operand === '-') {
-    return number1 - number2;
-  }
-  return number1 * number2;
+  return result;
 };
 const createGameValues = () => {
-  const number1 = generateRandomNumber(25);
-  const number2 = generateRandomNumber(10);
-  const operand = generateRandomOperand();
-  const currentValue = `${number1} ${operand} ${number2}`;
-  const correctAnswer = makeCalc(number1, number2, operand).toString();
-  return cons(currentValue, correctAnswer);
+  const number1 = generateRandomNumber(1, 25);
+  const number2 = generateRandomNumber(1, 10);
+  const operandIndex = generateRandomNumber(0, operands.length - 1);
+  const operand = operands[operandIndex];
+  const gameQuestion = `${number1} ${operand} ${number2}`;
+  const correctAnswer = makeCalc(number1, number2, operand);
+  return cons(gameQuestion, correctAnswer);
 };
 
 export default () => makeGame(rule, createGameValues);
