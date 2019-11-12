616e9fb0f58c4d4d849ee7dff182313e255d6e32
Olga Ioffe <yoffic@yahoo.com>
2019-11-12 12:16:30 +0300

    refactor progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,23 +1,31 @@
 import { cons } from '@hexlet/pairs';
 import makeGame, { generateRandomNumber } from '../index';
 
-const rule = 'What number is missing in the progression?\n';
-let hiddenNumber = 0;
-const createProgression = (hiddenPosition) => {
-  let currentProgressionNumber = generateRandomNumber(100);
+const rule = 'What number is missing in the progression?';
+const progressionLength = 10;
+const getHiddenNumber = (position, value) => {
+  const iter = (counter, acc) => {
+    if (counter === position) {
+      return acc;
+    }
+    return iter(counter + 1, acc + 2);
+  };
+  return iter(1, value);
+};
+const createProgression = (position, value) => {
+  let currentProgressionNumber = value;
   let progression = `${currentProgressionNumber}`;
-  if (hiddenPosition === 1) {
+  if (position === 1) {
     progression = '..';
   } else {
-    for (let i = 1; i < hiddenPosition - 1; i += 1) {
+    for (let i = 1; i < position - 1; i += 1) {
       currentProgressionNumber += 2;
       progression = `${progression} ${currentProgressionNumber}`;
     }
     progression = `${progression} ..`;
     currentProgressionNumber += 2;
   }
-  hiddenNumber = currentProgressionNumber;
-  for (let i = hiddenPosition; i < 10; i += 1) {
+  for (let i = position; i < progressionLength; i += 1) {
     currentProgressionNumber += 2;
     progression = `${progression} ${currentProgressionNumber}`;
   }
@@ -25,10 +33,11 @@ const createProgression = (hiddenPosition) => {
 };
 
 const createGameValues = () => {
-  const positionOfHiddenNumber = generateRandomNumber(10);
-  const currentValue = createProgression(positionOfHiddenNumber);
-  const correctAnswer = hiddenNumber.toString();
-  return cons(currentValue, correctAnswer);
+  const positionOfHiddenNumber = generateRandomNumber(1, progressionLength);
+  const startingValue = generateRandomNumber(1, 100);
+  const gameQuestion = createProgression(positionOfHiddenNumber, startingValue);
+  const correctAnswer = getHiddenNumber(positionOfHiddenNumber, startingValue).toString();
+  return cons(gameQuestion, correctAnswer);
 };
 
 export default () => makeGame(rule, createGameValues);
