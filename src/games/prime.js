47a0f0ff60bf588119ea8f4e8e2b352ce595ec52
Olga Ioffe <yoffic@yahoo.com>
2019-11-12 12:16:06 +0300

    refactor prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,22 +1,22 @@
 import { cons } from '@hexlet/pairs';
 import makeGame, { generateRandomNumber } from '../index';
 
-const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
-const isPrime = (num) => {
-  if (num % 2 === 0) {
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const isPrime = (value) => {
+  if (value % 2 === 0) {
     return false;
   }
   let divider = 1;
-  const upperBorder = Math.sqrt(num);
+  const upperBorder = Math.sqrt(value);
   for (let i = 3; i < upperBorder; i += 2) {
-    divider = num % i === 0 ? i : divider;
+    divider = value % i === 0 ? i : divider;
   }
   return divider === 1;
 };
 const createGameValues = () => {
-  const currentValue = generateRandomNumber(1000);
-  const correctAnswer = isPrime(currentValue) ? 'yes' : 'no';
-  return cons(currentValue, correctAnswer);
+  const gameQuestion = generateRandomNumber(1, 1000);
+  const correctAnswer = isPrime(gameQuestion) ? 'yes' : 'no';
+  return cons(gameQuestion, correctAnswer);
 };
 
 export default () => makeGame(rule, createGameValues);
