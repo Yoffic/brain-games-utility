845ee77bdb61b8b2ae9b3d7c182e2c4a8c5c5159
Olga Ioffe <yoffic@yahoo.com>
2019-11-13 22:36:35 +0300

    refactor progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,41 +2,26 @@ import { cons } from '@hexlet/pairs';
 import makeGame, { generateRandomNumber } from '../index';
 
 const rule = 'What number is missing in the progression?';
-const progressionLength = 10;
-const getHiddenNumber = (position, value) => {
-  const iter = (counter, acc) => {
-    if (counter === position) {
-      return acc;
+const createProgression = (startValue, hiddenValueIndex, step, progressLength) => {
+  let result = '';
+  for (let index = 0; index < progressLength; index += 1) {
+    if (index === hiddenValueIndex) {
+      result = `${result} ..`;
+    } else {
+      result = `${result} ${startValue + step * index}`;
     }
-    return iter(counter + 1, acc + 2);
-  };
-  return iter(1, value);
-};
-const createProgression = (position, value) => {
-  let currentProgressionNumber = value;
-  let progression = `${currentProgressionNumber}`;
-  if (position === 1) {
-    progression = '..';
-  } else {
-    for (let i = 1; i < position - 1; i += 1) {
-      currentProgressionNumber += 2;
-      progression = `${progression} ${currentProgressionNumber}`;
-    }
-    progression = `${progression} ..`;
-    currentProgressionNumber += 2;
   }
-  for (let i = position; i < progressionLength; i += 1) {
-    currentProgressionNumber += 2;
-    progression = `${progression} ${currentProgressionNumber}`;
-  }
-  return progression;
+  return result;
 };
 
 const createGameValues = () => {
-  const positionOfHiddenNumber = generateRandomNumber(1, progressionLength);
-  const startingValue = generateRandomNumber(1, 100);
-  const gameQuestion = createProgression(positionOfHiddenNumber, startingValue);
-  const correctAnswer = getHiddenNumber(positionOfHiddenNumber, startingValue).toString();
+  const progressLength = 10;
+  const lastIndex = progressLength - 1;
+  const hiddenValueIndex = generateRandomNumber(0, lastIndex);
+  const startValue = generateRandomNumber(1, 100);
+  const step = generateRandomNumber(1, 30);
+  const gameQuestion = createProgression(startValue, hiddenValueIndex, step, progressLength);
+  const correctAnswer = (startValue + step * hiddenValueIndex).toString();
   return cons(gameQuestion, correctAnswer);
 };
 
