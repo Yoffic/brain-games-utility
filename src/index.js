0268054be5dc5060cc7010f1a5ca377586cb8d99
Olga Ioffe <yoffic@yahoo.com>
2019-11-17 12:57:42 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from '@hexlet/pairs';
 
-export const generateRandomNumber = (min, max) => Math.floor(min + Math.random() * (max - min + 1));
-
-const makeGame = (rule, gameValues) => {
+const roundQuantity = 3;
+const makeGame = (rule, getGameValues) => {
   console.log('Welcome to the Brain Games!');
   console.log(`${rule}\n`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
-  const askQuestion = (round) => {
-    const roundQuantity = 3;
+  const makeGameRound = (round) => {
     if (round > roundQuantity) {
-      return `Congratulations, ${userName}!`;
+      console.log(`Congratulations, ${userName}!`);
+      return null;
     }
-    const currentGameValues = gameValues();
+    const currentGameValues = getGameValues();
     const questionValue = car(currentGameValues);
     const correctAnswer = cdr(currentGameValues);
     console.log(`Question: ${questionValue}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (correctAnswer === userAnswer) {
       console.log('Correct!');
-      return askQuestion(round + 1);
+      return makeGameRound(round + 1);
     }
-    return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+    return null;
   };
-  console.log(askQuestion(1));
+  makeGameRound(1);
 };
 
 export default makeGame;
