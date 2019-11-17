ece3a0deade6e9e4ebd7ef3fc77e1b69bf070e16
Olga Ioffe <yoffic@yahoo.com>
2019-11-17 13:14:58 +0300

    update utils export

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,3 @@
-export default (min, max) => Math.floor(min + Math.random() * (max - min + 1));
+const generateRandomNumber = (min, max) => Math.floor(min + Math.random() * (max - min + 1));
+
+export default generateRandomNumber;
