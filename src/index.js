d8a2d3af6d7b598f7a2e43423c6f9dd58fa41808
Olga Ioffe <yoffic@yahoo.com>
2019-11-19 23:42:52 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from '@hexlet/pairs';
 
-const roundQuantity = 3;
+const roundsCount = 3;
 const makeGame = (rule, getGameValues) => {
   console.log('Welcome to the Brain Games!');
   console.log(`${rule}\n`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
   const makeGameRound = (round) => {
-    if (round > roundQuantity) {
+    if (round > roundsCount) {
       console.log(`Congratulations, ${userName}!`);
-      return null;
+      return;
     }
     const currentGameValues = getGameValues();
-    const questionValue = car(currentGameValues);
+    const question = car(currentGameValues);
     const correctAnswer = cdr(currentGameValues);
-    console.log(`Question: ${questionValue}`);
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (correctAnswer === userAnswer) {
-      console.log('Correct!');
-      return makeGameRound(round + 1);
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-    return null;
+    console.log('Correct!');
+    makeGameRound(round + 1);
   };
-  makeGameRound(1);
+  return makeGameRound(1);
 };
 
 export default makeGame;
