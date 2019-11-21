33ea71abd20ac01937efe8ad9649f10dd0e518a1
Olga Ioffe <yoffic@yahoo.com>
2019-11-21 21:22:09 +0300

    refactor progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,22 +5,21 @@ import makeGame from '..';
 const rule = 'What number is missing in the progression?';
 const progressLength = 10;
 const lastIndex = progressLength - 1;
-const makeQuestion = (startValue, step, progressionLength, hiddenValueIndex) => {
-  let result = hiddenValueIndex === 0 ? '..' : `${startValue}`;
-  for (let i = 1; i < progressionLength; i += 1) {
-    result = i === hiddenValueIndex ? `${result} ..` : `${result} ${startValue + step * i}`;
+const makeQuestion = (start, step, length, hiddenValueIndex) => {
+  let result = '';
+  for (let i = 0; i < length; i += 1) {
+    result = i === hiddenValueIndex ? `${result} ..` : `${result} ${start + step * i}`;
   }
-  return result;
+  return result.trim();
 };
 
 const makeRound = () => {
   const hiddenValueIndex = generateRandomNumber(0, lastIndex);
-  const startValue = generateRandomNumber(1, 100);
+  const start = generateRandomNumber(1, 100);
   const step = generateRandomNumber(1, 30);
-  const hiddenValue = startValue + step * hiddenValueIndex;
-  const gameQuestion = makeQuestion(startValue, step, progressLength, hiddenValueIndex);
-  const correctAnswer = hiddenValue.toString();
-  return cons(gameQuestion, correctAnswer);
+  const question = makeQuestion(start, step, progressLength, hiddenValueIndex);
+  const correctAnswer = (start + step * hiddenValueIndex).toString();
+  return cons(question, correctAnswer);
 };
 
 export default () => makeGame(rule, makeRound);
